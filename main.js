82117c185093a732000e1248542be901237893b3
Tim Taubner <tim@voodle.de>
2012-07-15 20:29:01 +0200

    fixed floating radius

--- main.js
+++ main.js
@@ -147,7 +147,7 @@ var generateGrid = function (world, width, height, dot) {
 	    // { longitude: , latitude: , color: String (z.B. "#ff0088"), weight: [0..1], length: [in millis]}
 	    newEvent: function(event) {
 		var x = longToX(event.longitude), y = latToX(event.latitude),
-		    dot, i, j, radius = 3.2, length, distance, delay, nx, ny,
+		    dot, i, j, radius = 2, length, distance, delay, nx, ny,
 		    targetRadius = dotRadius,
 		    targetColor = new Color(event.color),
 		    createChangers = function (x, y, startColor, targetColor, startRadius, delay, length) {
@@ -170,7 +170,7 @@ var generateGrid = function (world, width, height, dot) {
 			    delay = Math.sqrt(distance) * event.length/radius;
 			    length = distance === 0 ? event.length : event.length/radius;
 			    if(length > 0) {
-				createChangers(nx, ny, dot.initial.color, new Color(targetColor.rgbString()).lighten(0.2*distance).clearer(0.2*distance), dot.initial.radius, delay, length);
+				createChangers(nx, ny, dot.initial.color, new Color(targetColor.rgbString()).lighten(0.32*distance).clearer(0.2*distance), dot.initial.radius, delay, length);
 			    }
 			}
 		    }
